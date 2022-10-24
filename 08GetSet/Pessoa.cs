526cbbad3061a9ff2222a666fa9ef854ccdb3776
Leila Contel <leeilacontell@gmail.com>
2022-10-24 11:20:38 -0300

    Get e Set

---
+++ 08GetSet/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Pessoa
+{
+    // Atributo
+    private string _nome;
+
+    // Get e Set
+    public string Nome
+    {
+        get{return _nome;}
+        set{_nome = value;}
+    }
+}
--- 08GetSet/Program.cs
+++ 08GetSet/Program.cs
@@ -10,11 +10,15 @@ namespace _08GetSet
             /*   ----------------------------------- SOBRE GET E SET -----------------------------------
                 set enviar informação para um atributo
                 get obtém a informação enviada
-                Encapsulamento evita deixar atributos de maneira publica
+                Usamos get e set para termo o encapsulamento onde evitamos o uso de atributos públicos
                 por exemplo você precisa enviar ou capturar uma informação sem deixar o atributo publico
             */
 
+            var pessoa = new Pessoa();
+
+            pessoa.Nome = "Leila"; // Set obtendo a informação
             
+            Console.WriteLine(pessoa.Nome); // _Get lendo a informação
         }
     }
 }
