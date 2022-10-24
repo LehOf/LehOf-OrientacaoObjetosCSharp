d5799b32b485e6bd658586c8c4b4e8b6fe46450a
Leila Contel <leeilacontell@gmail.com>
2022-10-24 15:45:55 -0300

    Atributos e Métodos estáticos aula 14

---
+++ 12Estático/Exemplo.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Exemplo
+{
+    // Atributo estático
+    public static string nome = "Leila";
+
+    // Método Estático
+    public static void Soma(int numero1, int numero2)
+    {
+        Console.WriteLine(numero1 + numero2);  
+    }
+}
---
+++ 12Estático/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12Estático
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Quando utilizamos o atributo ou método estático nâo precisamos instanciar o objeto
+            Console.Write("Digite o primeiro valor: ");
+            var numero1 = int.Parse(Console.ReadLine());
+             Console.Write("Digite o segundo valor: ");
+            var numero2 = int.Parse(Console.ReadLine());
+
+            // Atrbuindo valores ao parâmetro do método estático Soma()
+            Exemplo.Soma(numero1, numero2);
+
+            // Atributo estático
+            Console.WriteLine($"Olá: {Exemplo.nome}");
+        }
+    }
+}
