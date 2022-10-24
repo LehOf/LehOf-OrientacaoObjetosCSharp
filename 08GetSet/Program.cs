4cef0118d9c4d0b0c216796c0996e0295b7531eb
Leila Contel <leeilacontell@gmail.com>
2022-10-24 11:20:29 -0300

    Get e Set

---
+++ 08GetSet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*   ----------------------------------- SOBRE GET E SET -----------------------------------
+                set enviar informação para um atributo
+                get obtém a informação enviada
+                Encapsulamento evita deixar atributos de maneira publica
+                por exemplo você precisa enviar ou capturar uma informação sem deixar o atributo publico
+            */
+
+            
+        }
+    }
+}
